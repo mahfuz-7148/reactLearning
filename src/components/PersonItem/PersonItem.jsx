655ac0b7a088da3531193baded3ed6cd5366f69a
“mahfuz-7148” <mrahman7148@gmail.com>
2024-12-05 01:38:48 +0600

    'upp'

--- src/components/PersonItem/PersonItem.jsx
+++ src/components/PersonItem/PersonItem.jsx
@@ -7,12 +7,12 @@ const PersonItem = ({personnItem}) => {
     <div className='justify-items-center m-5'>
         <h1 className='text-3xl border-2 border-green-800 bg-red-200 p-2'>Person Item: {personnItem.length}</h1>
 
-        {
+         {
             personnItem.map(eachItem => <EachPerson
             key={eachItem.id}
             eachItem = {eachItem}
             ></EachPerson>)
-        }
+         }
     </div>
   )
 }
