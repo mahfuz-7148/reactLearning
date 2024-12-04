0a4877d3a1d4986b06664d4bd3715e05ee198f52
“mahfuz-7148” <mrahman7148@gmail.com>
2024-12-05 01:46:45 +0600

    'up'

--- src/components/PersonItem/PersonItem.jsx
+++ src/components/PersonItem/PersonItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import EachPerson from '../eachPerson/eachPerson'
+import EachPerson from '../EachPerson/EachPerson'
 
 const PersonItem = ({personnItem}) => {
     
@@ -7,12 +7,12 @@ const PersonItem = ({personnItem}) => {
     <div className='justify-items-center m-5'>
         <h1 className='text-3xl border-2 border-green-800 bg-red-200 p-2'>Person Item: {personnItem.length}</h1>
 
-         {
+        {
             personnItem.map(eachItem => <EachPerson
             key={eachItem.id}
             eachItem = {eachItem}
             ></EachPerson>)
-         }
+        }
     </div>
   )
 }
